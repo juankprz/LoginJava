d73b2ac162578d871bfe0c8955cea8599859b675
juan <juan@juan-HP-Mini-110-3800>
2017-01-15 17:59:58 -0500

    Clase conexion mysql---instancias y consultas con test

--- src/Persistecia/Conexion.java
+++ src/Persistecia/Conexion.java
@@ -5,10 +5,67 @@
  */
 package Persistecia;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author juan
  */
 public class Conexion {
+    Connection conexion = null;
+    Statement comando = null;
+    ResultSet registro;
+    private String sql;
     
+      public Connection conectar() {
+ 
+        try {
+            //Driver JDBC
+            Class.forName("com.mysql.jdbc.Driver");
+            //Nombre del servidor. localhost:3306 es la ruta y el puerto de la conexión MySQL
+            //panamahitek_text es el nombre que le dimos a la base de datos
+            String servidor = "jdbc:mysql://localhost:3306/Datos";
+            //El root es el nombre de usuario por default. No hay contraseña
+            String usuario = "root";
+            String pass = "juancamiloymarlys";
+            //Se inicia la conexión
+            conexion = (Connection) DriverManager.getConnection(servidor, usuario, pass);
+ 
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error en la conexión a la base de datos: " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion = null;
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error en la conexión a la base de datos: " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion = null;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error en la conexión a la base de datos: " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion = null;
+        } finally {
+        
+          return conexion;
+        }
+    }
+    public void Consultar( String sql) {
+        String nombre = null;
+        try {
+            Statement instancia =conectar().createStatement();
+            ResultSet rs = instancia.executeQuery(sql);
+            while(rs.next()){
+              nombre=rs.getString("Nombre");
+            }
+            JOptionPane.showMessageDialog(null,""+nombre);
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ 
+}
 }
